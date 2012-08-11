19530fa692fdc772fea01117288a88d847980c2b
Yawar Amin <yawar.amin@gmail.com>
2012-08-11 19:42:12 -0400

    Refactor the error caused by a failing assertion

--- Assert.cs
+++ Assert.cs
@@ -2,19 +2,21 @@
 
 namespace TDDUnit {
   class Assert {
+    private static void Fail(object expected, object actual) {
+      string message = string.Format("Expected: '{0}'; Actual: '{1}'", expected, actual);
+      Console.WriteLine(message);
+      throw new TestRunException(message);
+    }
+
     public static void Equal(object expected, object actual) {
       if (!expected.Equals(actual)) {
-        string message = string.Format("Expected: '{0}'; Actual: '{1}'", expected, actual);
-        Console.WriteLine(message);
-        throw new TestRunException(message);
+        Fail(expected, actual);
       }
     }
 
     public static void NotEqual(object expected, object actual) {
       if (expected.Equals(actual)) {
-        string message = string.Format("Expected: Not '{0}'; Actual: '{1}'", expected, actual);
-        Console.WriteLine(message);
-        throw new TestRunException(message);
+        Fail(expected, actual);
       }
     }
 
