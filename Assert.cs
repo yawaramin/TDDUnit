0de55caee492dee41aa9af5ef9cd8c393e2d6bc8
Yawar Amin <yawar.amin@gmail.com>
2012-08-17 23:42:23 -0400

    Fix bug in Assert.NotEqual error message
    This is what happens when you don't do TDD, I guess.

--- Assert.cs
+++ Assert.cs
@@ -2,21 +2,21 @@
 
 namespace TDDUnit {
   class Assert {
-    private static void Fail(object expected, object actual) {
-      string message = string.Format("Expected: '{0}'\nActual: '{1}'", expected, actual);
+    private static void Fail(object expected, object actual, bool equal) {
+      string message = string.Format("Expected: {2} '{0}'\nActual: '{1}'", expected, actual, (equal ? "" : "Not"));
       Console.WriteLine(message);
       throw new TestRunException(message);
     }
 
     public static void Equal(object expected, object actual) {
       if (!expected.Equals(actual)) {
-        Fail(expected, actual);
+        Fail(expected, actual, true);
       }
     }
 
     public static void NotEqual(object expected, object actual) {
       if (expected.Equals(actual)) {
-        Fail(expected, actual);
+        Fail(expected, actual, false);
       }
     }
 
